4aab5690d5c5b792e1e2d301e7566a9cb9f65b4b
Jeongjin Kim <jakekim4484@gmail.com>
2022-05-31 14:28:17 -0700

    new selenium

--- chrome.py
+++ chrome.py
@@ -1,10 +1,37 @@
 from selenium import webdriver
-# from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+import time
+# from webdriver_manager.chrome import ChromeDriverManager
 # driver = webdriver.Chrome(ChromeDriverManager().install())
 driver = webdriver.Chrome(executable_path="/Users/jakekim/Selenium/chromedriver")
-driver.get("http://www.google.com")
+driver.get("https://techwithtim.net")
+time.sleep(5)
 
 print(driver.title)
 print(driver.current_url)
-# driver.quit()
+
+#To find elements Id // Name // Class
+
+search = driver.find_element(By.CLASS_NAME,"search-field")
+search.send_keys("test")
+search.send_keys(Keys.RETURN)
+
+time.sleep(5)
+
+
+# bring all articles ID = "main"
+try:
+    main = WebDriverWait(driver,10).until(
+        EC.presence_of_element_located((By.ID, "main"))
+    )
+
+    articles = driver.find_element(By.ID, "main")
+    for article in articles:
+        header = driver.find_element(By.CLASS_NAME, "entry-summary")
+        print(header.text)
+finally:
+    driver.quit()
--- test.py
+++ test.py
@@ -1,11 +1,63 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+import time
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
-# driver = webdriver.Chrome('/Users/jakekim/Selenium/chromedriver')
+# driver = webdriver.Chrome(ChromeDriverManager().install()) // Driver in stall
+driver = webdriver.Chrome('/Users/jakekim/Selenium/chromedriver')
+# Check Chrome driver version. before start...
+
 driver.maximize_window()
-driver.get("http://www.google.com")
+driver.get("https://tamwood.com/careers/")
 
-print("Application title is ",driver.title)
-print("Application url is",driver.current_url)
+# print("Application title is ",driver.title)
+# print("Application url is",driver.current_url)
 # driver.quit()
+
+apply_page = driver.find_element_by_class_name("widget.edgt-button-widget")
+apply_page.click()
+time.sleep(3)
+
+contact_us = driver.find_elements_by_class_name("edgt-btn.edgt-btn-medium")
+contact_us[1].send_keys(Keys.ENTER)
+
+# After call new window, Python has to focus on new tab
+# first tab is window_handles[0] / Second tab is window_handles[-1] ....
+driver.switch_to.window(driver.window_handles[-2])
+time.sleep(3)
+
+firstname = driver.find_element(By.NAME, 'general-firstname')
+firstname.send_keys("firstname")
+time.sleep(2)
+
+lastname = driver.find_element(By.NAME, 'general-lastname')
+lastname.send_keys("lastname")
+time.sleep(2)
+
+
+
+
+
+
+
+
+# time.sleep(5)
+# link = driver.find_element_by_link_text("Camps")
+# link.click()
+
+
+# try:
+#     element = WebDriverWait(driver, 10).until(
+#         EC.presence_of_element((By.CLASS_NAME, "edgt-btn-text"))
+#     )
+#     element.click()
+#     # element = WebDriverWait(driver, 10).until(
+#     #     EC.presence_of_element_located((By.NAME, "careersfull-firstname"))
+#     # )
+#     # element.click()
+# finally:
+#     driver.quit()
